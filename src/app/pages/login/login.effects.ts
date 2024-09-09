23dcd41275569418a2561b396d77345270a0e3d4
Rindi1414 <rinditrisetyaningsih@gmail.com>
2024-09-09 09:34:55 +0700

    acara 2

--- src/app/pages/login/login.effects.ts
+++ src/app/pages/login/login.effects.ts
@@ -1,6 +1,6 @@
-import { Injectable } from "@angular/core";
+import { effect, Injectable } from "@angular/core";
 import { Actions, createEffect, ofType } from "@ngrx/effects";
-import { login, loginFail, loginSuccess, recoverPassword, recoverPasswordFail, recoverPasswordSuccess } from "./login.actions";
+import { login, loginFail, loginSuccess, recoverPassword, recoverPasswordFail, recoverPasswordSuccess } from "src/store/login/login.actions";
 import { catchError, map, switchMap } from "rxjs/operators";
 import { AuthService } from "src/app/services/auth/auth.service";
 import { of } from "rxjs";
@@ -8,13 +8,13 @@ import { of } from "rxjs";
 @Injectable()
 export class LoginEffects {
 
-  constructor(private actions$: Actions, private authService: AuthService){
+  constructor(private actions$: Actions, private authServices: AuthService) {
 
   }
 
   recoverPassword$ = createEffect(() => this.actions$.pipe(
     ofType(recoverPassword),
-    switchMap((payload: {email: string}) => this.authService.recoveryEmailPassword(payload.email).pipe(
+    switchMap((payload: {email: string}) => this.authServices.recoverEmailPassword(payload.email).pipe(
       map(() => recoverPasswordSuccess()),
       catchError(error => of(recoverPasswordFail({error})))
     ))
@@ -23,10 +23,10 @@ export class LoginEffects {
   login$ = createEffect(() => this.actions$.pipe(
     ofType(login),
     switchMap((payload: {email: string, password: string}) =>
-        this.authService.login(payload.email, payload.password).pipe(
-          map(user => loginSuccess({user})),
-          catchError(error => of(loginFail({error})))
-        )
+      this.authServices.login(payload.email, payload.password).pipe(
+        map(user => loginSuccess({user})),
+        catchError(error => of(loginFail({error})))
       )
-    ))
-  }
+    )
+  ))
+}
